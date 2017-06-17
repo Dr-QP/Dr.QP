7d451bb1fd704178b8d640e46d9cdd9aae30986c
Anton Matosov <anton.matosov@gmail.com>
2017-06-17 15:50:30 -0700

    Implemented Unix/OSX/Windows serial protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 
+
 int main()
 {
 // /dev/cu.usbserial-A104D3XB
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+
+    return 0;
 }
--- tests/TestsMain.cpp
+++ tests/TestsMain.cpp
@@ -2,7 +2,40 @@
 // Created by Anton Matosov on 6/15/17.
 //
 
+#include "UnixSerial.h"
+//#include <boost/asio.hpp>
+#include <boost/thread.hpp>
+
+void testSerialPort()
+{
+    UnixSerial unixSerial("/dev/cu.SLAB_USBtoUART");
+    unixSerial.begin(115200);
+
+    unixSerial.write('a');
+    unixSerial.write('b');
+    unixSerial.write('c');
+    unixSerial.write('d');
+    unixSerial.write('e');
+    unixSerial.write('f');
+    unixSerial.write('g');
+    unixSerial.write('\n');
+
+//    using namespace boost::asio;
+//    io_service portService;
+//    serial_port serial(portService, "/dev/cu.SLAB_USBtoUART");
+//
+//    serial.set_option(serial_port_base::baud_rate(115200));
+//
+//    std::string s = "abcdefg\n";
+//    boost::asio::async_write(serial, boost::asio::buffer(s.c_str(), s.size()), transfer_all());
+//
+//
+    boost::this_thread::sleep(boost::posix_time::milliseconds(100));
+}
+
 int main()
 {
-  return 0;
+    testSerialPort();
+
+    return 0;
 }
