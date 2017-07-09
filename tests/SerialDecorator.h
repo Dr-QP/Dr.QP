ffdbd5a799dbe3290345ea37a18391a5cf71bf77
Anton Matosov <anton.matosov@gmail.com>
2017-07-09 13:37:29 -0700

    Rework project to use conan for dependent libs

--- tests/SerialDecorator.h
+++ tests/SerialDecorator.h
@@ -4,6 +4,7 @@
 
 #pragma once 
 
+#include <cstddef>
 #include "SerialProtocol.h"
 
 class SerialDecorator : public SerialProtocol
--- tests/SerialPlayer.h
+++ tests/SerialPlayer.h
@@ -4,6 +4,7 @@
 
 #pragma once 
 
+#include <cstddef>
 #include "SerialProtocol.h"
 #include <deque>
 #import "RecordingProxy.h"
