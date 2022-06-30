43898c6fc6ccf0075b495bf4ba21520f4a76a125
Anton Matosov <anton.matosov@gmail.com>
2022-06-30 02:17:58 -0700

    print well

--- src/ReadAll.cpp
+++ src/ReadAll.cpp
@@ -11,17 +11,19 @@ int main() {
   TcpSerial servoSerial("192.168.1.136", 2022);
 
   for (const auto &leg : kAllLegServoIds) {
+    int servoIndexInLeg = 0;
     for (const auto servoId : leg) {
       XYZrobotServo servo(servoSerial, servoId);
 
       XYZrobotServoStatus status = servo.readStatus();
       if (servo.getLastError()) {
-        std::cerr << legNameForServo(servoId) << " servo: " << servoId
+        std::cerr << legNameForServo(servoId) << " servo: " << (int)servoId
                   << " error reading status: " << servo.getLastError() << "\n";
       } else {
-        std::cout << legNameForServo(servoId) << "\tservo: " << servoId << ":\t"
-                  << (int)status.position << "\n";
+        std::cout << legNameForServo(servoId) << "\tservo: " << (int)servoId << ": "
+                  << (int)status.position << "\t neutral: " << kNeutralPose[kServoIdToLeg[servoId]][servoIndexInLeg] << "\n";
       }
+      ++servoIndexInLeg;
     }
     std::cout << "\n";
   }
