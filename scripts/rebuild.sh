92fc51a1cd87186c789e128e86401741a1758188
Anton Matosov <anton.matosov@gmail.com>
2022-07-05 07:15:03 -0700

    feature/full-conda some debug info

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-# set -x
+set -x
 
 script_path="${BASH_SOURCE[0]}"
 script_dir=$(dirname $script_path)
@@ -8,9 +8,10 @@ root_dir=$(dirname $script_dir)
 
 cd "$root_dir"
 
+env | sort
 which cmake
 which ninja
-which clang
+which $CC
 
 rm -rf build/
 mkdir build/
