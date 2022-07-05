62ec893bd9c1b2c350307eca1d6914fece2fd5d9
Anton Matosov <anton.matosov@gmail.com>
2022-07-05 01:00:35 -0700

    link instead of copy

--- git/install-hooks.sh
+++ git/install-hooks.sh
@@ -6,4 +6,4 @@ script_path="${BASH_SOURCE[0]}"
 script_dir=$(dirname $script_path)
 git_dir=$(dirname $script_dir)/.git
 
-find "$script_dir/hooks" -type f | xargs -I{} cp -f "{}" "$git_dir/hooks/"
+find "$script_dir/hooks" -type f | xargs -I{} ln -s -f "{}" "$git_dir/hooks/"
