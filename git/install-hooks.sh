5aecc941f3473485029e92ec4ebb095b63e32ba5
Anton Matosov <anton.matosov@gmail.com>
2022-07-05 01:24:02 -0700

    feature/doctest move to scripts more sophisticated install-hooks with better output

--- git/install-hooks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-
-script_path="${BASH_SOURCE[0]}"
-script_dir=$(dirname $script_path)
-git_dir=$(dirname $script_dir)/.git
-
-find "$script_dir/hooks" -type f | xargs -I{} ln -s -f "{}" "$git_dir/hooks/"
