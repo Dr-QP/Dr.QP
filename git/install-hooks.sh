378b4798d95254b599bb6e3a647108cccb7f55dd
Anton Matosov <anton.matosov@gmail.com>
2022-07-05 00:58:11 -0700

    add hook to include branch name in the commit message

---
+++ git/install-hooks.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+script_path="${BASH_SOURCE[0]}"
+script_dir=$(dirname $script_path)
+git_dir=$(dirname $script_dir)/.git
+
+find "$script_dir/hooks" -type f | xargs -I{} cp -f "{}" "$git_dir/hooks/"
