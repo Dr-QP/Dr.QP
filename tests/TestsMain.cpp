3e23492eff671fe125effb113004de50d1480615
Anton Matosov <anton.matosov@gmail.com>
2022-07-05 01:36:02 -0700

    feature/doctest Migrate to doctest

--- tests/TestsMain.cpp
+++ tests/TestsMain.cpp
@@ -1,6 +1,3 @@
-//
-// Created by Anton Matosov on 6/15/17.
-//
 
-// #define CATCH_CONFIG_MAIN
-// #include <catch2/catch_test_macros.hpp>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
