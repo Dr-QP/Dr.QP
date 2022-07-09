a4efbe2f90c515d995d050cb4caf86e705769c23
Anton Matosov <anton.matosov@gmail.com>
2022-07-08 19:59:30 -0700

    feature/to-ros-package add ROS_DOMAIN_ID add bash version of helper scripts

--- scripts/ros/ros.fish
+++ scripts/ros/ros.fish
@@ -1,9 +1,14 @@
+#!/usr/bin/env fish
+# -*- coding: utf-8 -*-
+
 
 set -gx MAMBA_NO_BANNER 1
 
 function ros2_activate
+    set -gx ROS_DOMAIN_ID 58
     conda activate ros_env
     bass source $CONDA_PREFIX/setup.bash
+
 end
 
 function ros2_ws
