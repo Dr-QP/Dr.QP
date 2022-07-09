aee98c8ab94f45d52646c9795f32bbc72f244014
Anton Matosov <anton.matosov@gmail.com>
2022-07-09 00:20:15 -0700

    feature/to-ros-package ros autocomplete

--- scripts/ros/ros.fish
+++ scripts/ros/ros.fish
@@ -8,7 +8,10 @@ function ros2_activate
     set -gx ROS_DOMAIN_ID 3
     conda activate ros_env
     bass source $CONDA_PREFIX/setup.bash
-
+    register-python-argcomplete --shell fish ros2 | source
+    register-python-argcomplete --shell fish colcon | source
+    register-python-argcomplete --shell fish rosidl | source
+    register-python-argcomplete --shell fish ament_index | source
 end
 
 function ros2_ws
