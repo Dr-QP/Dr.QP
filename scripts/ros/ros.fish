e98f6db94fdab41b709a5506d33c6e2f6fa2d3ad
Anton Matosov <anton.matosov@gmail.com>
2022-07-10 00:54:42 -0700

    feature/to-ros-package more scripts

--- scripts/ros/ros.fish
+++ scripts/ros/ros.fish
@@ -1,11 +1,13 @@
 #!/usr/bin/env fish
 # -*- coding: utf-8 -*-
 
+set SCRIPT_DIR (dirname (status --current-filename))
 
 set -gx MAMBA_NO_BANNER 1
 
 function ros2_activate
     set -gx ROS_DOMAIN_ID 3
+    set -gx PIP_CONFIG_FILE $SCRIPT_DIR/pip.conf
     conda activate ros_env
     bass source $CONDA_PREFIX/setup.bash
     register-python-argcomplete --shell fish ros2 | source
@@ -16,5 +18,9 @@ end
 
 function ros2_ws
     ros2_activate
-    bass source setup.bash
+    bass source install/local_setup.bash
+end
+
+function rosdep_src
+    rosdep install --from-paths src --ignore-src -r -y
 end
