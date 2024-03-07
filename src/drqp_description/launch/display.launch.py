cd2044dafc3efefd782a5d121d69e4aed09b1497
Anton Matosov <anton.matosov@gmail.com>
2024-03-06 23:50:18 -0800

    uncomment joint state publishers

--- src/drqp_description/launch/display.launch.py
+++ src/drqp_description/launch/display.launch.py
@@ -61,7 +61,7 @@ def generate_launch_description():
         rviz_frame_arg,
         external_urdf_loc_arg,
         use_sim_time_arg,
-        # joint_state_publisher_node,
-        # joint_state_publisher_gui_node,
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
         rviz_node
     ])
