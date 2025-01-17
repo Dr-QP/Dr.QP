0f5050bab38fdd3a429269a80401ac77f5d3fa43
Anton Matosov <anton.matosov@gmail.com>
2025-01-16 18:42:30 -0800

    colcon test passes now

--- src/drqp_description/launch/display.launch.py
+++ src/drqp_description/launch/display.launch.py
@@ -1,12 +1,31 @@
+# Copyright (c) 2022-2025 Anton Matosov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 from ament_index_python.packages import get_package_share_path
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
@@ -15,19 +34,24 @@ def generate_launch_description():
 
     gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
                                     description='Flag to enable joint_state_publisher_gui')
-    external_urdf_loc_arg = DeclareLaunchArgument(name='external_urdf_loc', default_value="",
-                                                  description='Absolute path additional urdf file to include in the model')
+    external_urdf_loc_arg = DeclareLaunchArgument(name='external_urdf_loc', default_value='',
+                                                  description='Absolute path additional urdf \
+                                                    file to include in the model')
 
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_sim_time_arg = DeclareLaunchArgument(
             name='use_sim_time',
             default_value='true',
             description='Use sim time if true')
-    rviz_frame_arg = DeclareLaunchArgument(name='rviz_frame', default_value='dr_qp/base_link',
+    rviz_frame_arg = DeclareLaunchArgument(name='rviz_frame',
+                                           default_value='dr_qp/base_link',
                                            description='Base model frame in rviz')
-    rviz_arg = DeclareLaunchArgument(name='rviz', default_value='true', choices=['true', 'false'],
+    rviz_arg = DeclareLaunchArgument(name='rviz',
+                                     default_value='true',
+                                     choices=['true', 'false'],
                                      description='Flag to enable rviz')
-    rvizconfig_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+    rvizconfig_arg = DeclareLaunchArgument(name='rvizconfig',
+                                           default_value=str(default_rviz_config_path),
                                            description='Absolute path to rviz config file')
 
     # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
--- src/drqp_description/launch/rsp.launch.py
+++ src/drqp_description/launch/rsp.launch.py
@@ -1,17 +1,37 @@
+# Copyright (c) 2022-2025 Anton Matosov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import os
 
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.substitutions import LaunchConfiguration, Command
-from launch.actions import DeclareLaunchArgument
-from launch_ros.actions import Node
 
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command, LaunchConfiguration
+
+from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
-
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_ros2_control = LaunchConfiguration('use_ros2_control')
@@ -20,8 +40,11 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('drqp_description'))
     xacro_file = os.path.join(pkg_path, 'urdf', 'dr_qp.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file).toxml()
-    robot_description_config = ParameterValue(Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time]),
-                                              value_type=str)
+    robot_description_config = ParameterValue(Command([
+        'xacro ', xacro_file,
+        ' use_ros2_control:=', use_ros2_control,
+        ' sim_mode:=', use_sim_time
+        ]), value_type=str)
 
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config,
