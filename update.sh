878dab95fdf58590130a9247fe2d42aee0dab158
Anton Matosov <anton.matosov@gmail.com>
2025-01-27 14:37:11 -0800

    up script

--- update.sh
+++ update.sh
@@ -5,5 +5,7 @@ script_dir=$(dirname "$0")
 cd $script_dir
 cd $(git rev-parse --show-toplevel)
 
-git subtree add --prefix="$script_dir/packages/drqp_rapidjson/include/" https://github.com/Dr-QP/rapidjson.git drqp_rapidjson --squash
-git subtree pull --prefix="$script_dir/packages/drqp_rapidjson/include/"
+prefix=packages/drqp_rapidjson/include/
+
+git subtree add --prefix="$prefix" https://github.com/Dr-QP/rapidjson.git drqp_rapidjson --squash
+git subtree pull --prefix="$prefix"
