b8ae4a5ca4d70ef2908a9ed8031fe8d81eddf803
Anton Matosov <anton.matosov@gmail.com>
2025-01-31 02:09:19 -0800

    reg other commands

--- scripts/ros/ros.fish
+++ scripts/ros/ros.fish
@@ -1,30 +1,34 @@
 
 function register_argcomplete
-    #Produced by: register-python-argcomplete --shell fish ros2
-    function __fish_ros2_complete
-        set -x _ARGCOMPLETE 1
-        set -x _ARGCOMPLETE_DFS \t
-        set -x _ARGCOMPLETE_IFS \n
-        set -x _ARGCOMPLETE_SUPPRESS_SPACE 1
-        set -x _ARGCOMPLETE_SHELL fish
-        set -x COMP_LINE (commandline -p)
-        set -x COMP_POINT (string length (commandline -cp))
-        set -x COMP_TYPE
-        if set -q _ARC_DEBUG
-            ros2 8>&1 9>&2 1>&9 2>&1
-        else
-            ros2 8>&1 9>&2 1>/dev/null 2>&1
-        end
-    end
-    complete --command ros2 -f -a '(__fish_ros2_complete)'
+  #Produced by: register-python-argcomplete --shell fish ros2
+  function __fish_complete
+      set -l cmd $argv[1]
+      set -x _ARGCOMPLETE 1
+      set -x _ARGCOMPLETE_DFS \t
+      set -x _ARGCOMPLETE_IFS \n
+      set -x _ARGCOMPLETE_SUPPRESS_SPACE 1
+      set -x _ARGCOMPLETE_SHELL fish
+      set -x COMP_LINE (commandline -p)
+      set -x COMP_POINT (string length (commandline -cp))
+      set -x COMP_TYPE
+      if set -q _ARC_DEBUG
+          $cmd 8>&1 9>&2 1>&9 2>&1
+      else
+          $cmd 8>&1 9>&2 1>/dev/null 2>&1
+      end
+  end
+  complete --command ros2 -f -a '(__fish_complete ros2)'
+  complete --command colcon -f -a '(__fish_complete colcon)'
+  complete --command rosidl -f -a '(__fish_complete rosidl)'
+  complete --command ament_index -f -a '(__fish_complete ament_index)'
 end
 
 function ros2_activate
-    bass source /opt/ros/humble/setup.bash
-    register_argcomplete
+  bass source /opt/ros/humble/setup.bash
+  register_argcomplete
 end
 
 function ros2_ws
-    bass source ./install/setup.bash
-    register_argcomplete
+  bass source ./install/setup.bash
+  register_argcomplete
 end
