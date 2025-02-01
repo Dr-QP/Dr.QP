955f81f310d09c52fbb4cefb179503e33efe8aa9
Anton Matosov <anton.matosov@gmail.com>
2025-01-31 16:51:43 -0800

    Add sketch for ament_clang_tidy

--- scripts/ros/cpp-reformat.sh
+++ scripts/ros/cpp-reformat.sh
@@ -4,3 +4,7 @@ script_dir=$(dirname $0)
 source "$script_dir/__utils.sh"
 
 ament_clang_format --config .clang-format --reformat "$sources_dir/"
+
+# It runs, but doesn't fix anything and is very slow
+# ament_clang_tidy --fix-errors "$root_dir/build/compile_commands.json"
+# --config .clang-tidy
