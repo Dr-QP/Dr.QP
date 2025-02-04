653805e4ac060a362a5e1054685c1262838d20ca
Anton Matosov <anton.matosov@gmail.com>
2025-02-03 22:19:28 -0800

    switch to lambda

--- packages/drqp_a1_16_driver/src/Node.cpp
+++ packages/drqp_a1_16_driver/src/Node.cpp
@@ -16,28 +16,18 @@
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
-
 class MinimalPublisher : public rclcpp::Node
 {
-  public:
-    MinimalPublisher()
-    : Node("drqp_a1_16_driver")
-    {
-      publisher_ = this->create_publisher<drqp_interfaces::msg::MultiSyncPositionCommand>("pose", 10);
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
-    }
+public:
+  MinimalPublisher() : Node("drqp_a1_16_driver")
+  {
+    publisher_ = this->create_publisher<drqp_interfaces::msg::MultiSyncPositionCommand>("pose", 10);
 
-  private:
-    void timer_callback()
-    {
+    timer_ = this->create_wall_timer(500ms, [this]() {
       auto pose = drqp_interfaces::msg::MultiSyncPositionCommand{};
       std::unique_ptr<SerialProtocol> servoSerial;
       servoSerial = std::make_unique<TcpSerial>("192.168.0.181", "2022");
-      for (uint8_t servoId = 1; servoId <= 18; ++servoId)
-      {
+      for (uint8_t servoId = 1; servoId <= 18; ++servoId) {
         XYZrobotServo servo(*servoSerial, servoId);
 
         XYZrobotServoStatus status = servo.readStatus();
@@ -49,13 +39,14 @@ class MinimalPublisher : public rclcpp::Node
         pose.positions.push_back(pos);
       }
       publisher_->publish(pose);
-    }
+    });
+  }
 
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<drqp_interfaces::msg::MultiSyncPositionCommand>::SharedPtr publisher_;
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<drqp_interfaces::msg::MultiSyncPositionCommand>::SharedPtr publisher_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
