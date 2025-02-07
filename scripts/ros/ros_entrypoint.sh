da958b1ddcfca631b6c560c88c222ba700391c2c
Anton Matosov <anton.matosov@gmail.com>
2025-02-06 17:00:54 -0800

    bashrc update is already there in the docker file

--- scripts/ros/ros_entrypoint.sh
+++ scripts/ros/ros_entrypoint.sh
@@ -3,11 +3,10 @@
 set -e
 
 # setup ros2 environment
-source /opt/ros/"$ROS_DISTRO"/setup.bash --
-source ~/ros2_ws/install/setup.bash --
+source "/opt/ros/$ROS_DISTRO/setup.bash" --
 
-# add sourcing to .bashrc
-echo "source '/opt/ros/$ROS_DISTRO/setup.bash'" >> ~/.bashrc
-echo "source '~/ros2_ws/install/setup.bash'" >> ~/.bashrc
+if [[ -f "~/ros2_ws/install/setup.bash" ]]; then
+  source ~/ros2_ws/install/setup.bash --
+fi
 
 exec "$@"
