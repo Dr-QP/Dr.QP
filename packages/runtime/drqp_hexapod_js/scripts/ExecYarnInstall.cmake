dc9847d4038afec6f9128f6525f3f26661b256cd
Anton Matosov <anton.matosov@gmail.com>
2025-02-26 15:22:55 -0800

    configure or turn off some lints to make tests pass

--- packages/runtime/drqp_hexapod_js/scripts/ExecYarnInstall.cmake
+++ packages/runtime/drqp_hexapod_js/scripts/ExecYarnInstall.cmake
@@ -1,20 +1,7 @@
 set(PROJECT_NAME drqp_hexapod_js)
 
 execute_process(
-  COMMAND ${CMAKE_CURRENT_LIST_DIR}/yarn_ws_install.sh ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/local_setup.bash
-  WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
-
-  COMMAND_ERROR_IS_FATAL ANY
-  TIMEOUT 300
-
-  ECHO_OUTPUT_VARIABLE
-  ECHO_ERROR_VARIABLE
-  COMMAND_ECHO STDOUT
-)
-
-# For sequential execution of multiple commands use multiple execute_process calls each with a single COMMAND argument
-execute_process(
-  COMMAND find . -type d -name node_modules -exec touch {}/COLCON_IGNORE ";"
+  COMMAND bash -c "source ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/local_setup.bash yarn install"
   WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
 
   COMMAND_ERROR_IS_FATAL ANY
