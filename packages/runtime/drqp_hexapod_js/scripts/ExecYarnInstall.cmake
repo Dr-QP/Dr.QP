8f8e0987ea6deee3028ed4b61a613021d084107f
Anton Matosov <anton.matosov@gmail.com>
2025-02-26 14:41:28 -0800

    fix yarn install in the install dir

--- packages/runtime/drqp_hexapod_js/scripts/ExecYarnInstall.cmake
+++ packages/runtime/drqp_hexapod_js/scripts/ExecYarnInstall.cmake
@@ -1,10 +1,15 @@
+set(PROJECT_NAME drqp_hexapod_js)
+
 execute_process(
-  COMMAND yarn install
+  COMMAND ${CMAKE_CURRENT_LIST_DIR}/yarn_ws_install.sh ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/local_setup.bash
   WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
 
   COMMAND_ERROR_IS_FATAL ANY
-  COMMAND_ECHO STDOUT
   TIMEOUT 300
+
+  ECHO_OUTPUT_VARIABLE
+  ECHO_ERROR_VARIABLE
+  COMMAND_ECHO STDOUT
 )
 
 # For sequential execution of multiple commands use multiple execute_process calls each with a single COMMAND argument
@@ -13,6 +18,9 @@ execute_process(
   WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
 
   COMMAND_ERROR_IS_FATAL ANY
-  COMMAND_ECHO STDOUT
   TIMEOUT 300
+
+  ECHO_OUTPUT_VARIABLE
+  ECHO_ERROR_VARIABLE
+  COMMAND_ECHO STDOUT
 )
