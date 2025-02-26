3147116ce98bbb10191819431950fd9f32511dc9
Anton Matosov <anton.matosov@gmail.com>
2025-02-26 13:07:13 -0800

    remove unused

--- packages/runtime/drqp_hexapod_js/launch/hexapod.launch.py
+++ packages/runtime/drqp_hexapod_js/launch/hexapod.launch.py
@@ -3,14 +3,10 @@ import os
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
-
     drqp_hexapod_js_dir = get_package_share_directory("drqp_hexapod_js")
 
     web_launch = IncludeLaunchDescription(
