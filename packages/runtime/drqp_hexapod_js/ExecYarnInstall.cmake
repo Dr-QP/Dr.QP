9b32be86b68bfb94e554ae81e174af3112829b6a
Anton Matosov <anton.matosov@gmail.com>
2025-02-26 13:41:57 -0800

    fix

--- packages/runtime/drqp_hexapod_js/ExecYarnInstall.cmake
+++ packages/runtime/drqp_hexapod_js/ExecYarnInstall.cmake
@@ -1,6 +1,6 @@
 execute_process(
   COMMAND yarn install
-  COMMAND find . -fstype dir -name node_modules -exec touch {}/COLCON_IGNORE ";"
+  COMMAND find . -type d -name node_modules -exec touch {}/COLCON_IGNORE ";"
   WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
 
   COMMAND_ERROR_IS_FATAL ANY
