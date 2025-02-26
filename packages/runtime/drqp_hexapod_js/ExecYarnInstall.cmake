6cc9769c4bc6c0528352d05b6a3d484db783639b
Anton Matosov <anton.matosov@gmail.com>
2025-02-26 14:23:28 -0800

    For sequential execution of multiple commands use multiple execute_process calls each with a single COMMAND argument

--- packages/runtime/drqp_hexapod_js/ExecYarnInstall.cmake
+++ packages/runtime/drqp_hexapod_js/ExecYarnInstall.cmake
@@ -1,5 +1,14 @@
 execute_process(
   COMMAND yarn install
+  WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
+
+  COMMAND_ERROR_IS_FATAL ANY
+  COMMAND_ECHO STDOUT
+  TIMEOUT 300
+)
+
+# For sequential execution of multiple commands use multiple execute_process calls each with a single COMMAND argument
+execute_process(
   COMMAND find . -type d -name node_modules -exec touch {}/COLCON_IGNORE ";"
   WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
 
