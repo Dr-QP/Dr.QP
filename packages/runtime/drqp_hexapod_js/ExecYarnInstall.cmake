8fbba4c6ab20eee74194f2e9054184fa4f4f2b0e
Anton Matosov <anton.matosov@gmail.com>
2025-02-26 13:37:32 -0800

    hardening

--- packages/runtime/drqp_hexapod_js/ExecYarnInstall.cmake
+++ packages/runtime/drqp_hexapod_js/ExecYarnInstall.cmake
@@ -2,4 +2,8 @@ execute_process(
   COMMAND yarn install
   COMMAND find . -fstype dir -name node_modules -exec touch {}/COLCON_IGNORE ";"
   WORKING_DIRECTORY ${CMAKE_INSTALL_PREFIX}/share/${PROJECT_NAME}/dist
+
+  COMMAND_ERROR_IS_FATAL ANY
+  COMMAND_ECHO STDOUT
+  TIMEOUT 300
 )
