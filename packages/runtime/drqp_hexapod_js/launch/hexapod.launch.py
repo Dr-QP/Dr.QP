0a5e4a8760eb5cc46746f976ce62bc5d54c43e32
Anton Matosov <anton.matosov@gmail.com>
2025-02-26 14:17:16 -0800

    fix all ament errors

--- packages/runtime/drqp_hexapod_js/launch/hexapod.launch.py
+++ packages/runtime/drqp_hexapod_js/launch/hexapod.launch.py
@@ -1,24 +1,44 @@
+# Copyright (c) 2017-2025 Anton Matosov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the 'Software'), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
 import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    drqp_hexapod_js_dir = get_package_share_directory("drqp_hexapod_js")
+    drqp_hexapod_js_dir = get_package_share_directory('drqp_hexapod_js')
 
     web_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(drqp_hexapod_js_dir, "launch/web.launch.py")
+            os.path.join(drqp_hexapod_js_dir, 'launch/web.launch.py')
         )
     )
 
-    drqp_a1_16_driver_dir = get_package_share_directory("drqp_a1_16_driver")
+    drqp_a1_16_driver_dir = get_package_share_directory('drqp_a1_16_driver')
     pose_setter_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(drqp_a1_16_driver_dir, "launch/pose_setter.launch.py")
+            os.path.join(drqp_a1_16_driver_dir, 'launch/pose_setter.launch.py')
         )
     )
 
