6ecdf11245c0cfd104644e77c55ac392629a165b
Anton Matosov <anton.matosov@gmail.com>
2025-03-07 14:06:06 -0800

    per target coverage

--- packages/cmake/ClangCoverage.cmake
+++ packages/cmake/ClangCoverage.cmake
@@ -1,18 +1,26 @@
 # Enable clang's Source based code coverage
 
 option(DRQP_ENABLE_COVERAGE "Enable coverage" OFF)
-if (NOT DRQP_ENABLE_COVERAGE)
-  return()
-endif()
-
-if (NOT BUILD_TESTING)
-  return()
-endif()
 
 if (NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   message(FATAL_ERROR "Coverage is only supported with Clang")
 endif()
 
-message(STATUS "Enabling coverage")
-add_compile_options(-fprofile-instr-generate -fcoverage-mapping)
+function(drqp_enable_coverage TARGET)
+  if (NOT DRQP_ENABLE_COVERAGE)
+    return()
+  endif()
 
+  message(STATUS "Enabling coverage for ${TARGET}")
+  target_compile_options(${TARGET} PRIVATE -fprofile-instr-generate -fcoverage-mapping)
+endfunction()
+
+
+function(drqp_test_enable_coverage TARGET)
+  if (NOT DRQP_ENABLE_COVERAGE)
+    return()
+  endif()
+
+  message(STATUS "Enabling coverage for ${TARGET}")
+  target_link_options(${TARGET} PRIVATE -fprofile-instr-generate -fcoverage-mapping)
+endfunction()
