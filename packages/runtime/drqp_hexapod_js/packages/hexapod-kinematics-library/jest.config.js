53f911de6d39796ae7c16bc27e839fce0e7287b5
Anton Matosov <anton.matosov@gmail.com>
2025-03-06 16:27:42 -0800

    simplify configs for the local run

--- packages/runtime/drqp_hexapod_js/packages/hexapod-kinematics-library/jest.config.js
+++ packages/runtime/drqp_hexapod_js/packages/hexapod-kinematics-library/jest.config.js
@@ -3,8 +3,6 @@
 const config = {
   reporters: [
     'default',
-    ['jest-junit', { outputDirectory: 'jest_test_results', outputName: 'report-jest.xunit.xml' }],
-    ['github-actions', { silent: false }],
   ],
   coverageReporters: ['lcov', 'json'],
   collectCoverage: true,
--- packages/runtime/drqp_hexapod_js/packages/hexapod/jest-server.config.js
+++ packages/runtime/drqp_hexapod_js/packages/hexapod/jest-server.config.js
@@ -3,8 +3,6 @@
 const config = {
   reporters: [
     'default',
-    ['jest-junit', { outputDirectory: 'jest_test_results', outputName: 'server-report-jest.xunit.xml' }],
-    ['github-actions', { silent: false }],
   ],
   coverageReporters: ['lcov'],
   collectCoverage: true,
