a2030921b7fe7d2c4bc2c4bbfd99505da0ef966a
Anton Matosov <anton.matosov@gmail.com>
2025-03-07 16:27:24 -0800

    better names and a bit of comm comments

--- packages/cmake/ClangCoverage.cmake
+++ packages/cmake/ClangCoverage.cmake
@@ -6,21 +6,40 @@ if (NOT CMAKE_CXX_COMPILER_ID MATCHES "Clang")
   message(FATAL_ERROR "Coverage is only supported with Clang")
 endif()
 
-function(drqp_enable_coverage TARGET)
+set(CLANG_FLAGS -fprofile-instr-generate -fcoverage-mapping)
+
+# Enable coverage for C++ library
+# by adding needed compilation flags
+function(drqp_library_enable_coverage TARGET)
   if (NOT DRQP_ENABLE_COVERAGE)
     return()
   endif()
 
-  message(STATUS "Enabling coverage for ${TARGET}")
-  target_compile_options(${TARGET} PRIVATE -fprofile-instr-generate -fcoverage-mapping)
+  message(STATUS "DRQP: Enabling coverage for source code of ${TARGET} library")
+  target_compile_options(${TARGET} PRIVATE ${CLANG_FLAGS})
 endfunction()
 
-
+# Enable coverage for C++ test binary
+# by adding needed linker flags flags
+# code from the test binary will be ignored
 function(drqp_test_enable_coverage TARGET)
   if (NOT DRQP_ENABLE_COVERAGE)
     return()
   endif()
 
-  message(STATUS "Enabling coverage for ${TARGET}")
-  target_link_options(${TARGET} PRIVATE -fprofile-instr-generate -fcoverage-mapping)
+  message(STATUS "DRQP: Enabling coverage for ${TARGET} test target, code from the test binary will be ignored")
+  target_link_options(${TARGET} PRIVATE ${CLANG_FLAGS})
+endfunction()
+
+# Enable coverage for C++ executable
+# by adding needed compilation and linker flags
+# code from this executable will be included
+function(drqp_executable_enable_coverage TARGET)
+  if (NOT DRQP_ENABLE_COVERAGE)
+    return()
+  endif()
+
+  message(STATUS "DRQP: Enabling coverage for executable target ${TARGET}, code from this executable will be included")
+  target_compile_options(${TARGET} PRIVATE ${CLANG_FLAGS})
+  target_link_options(${TARGET} PRIVATE ${CLANG_FLAGS})
 endfunction()
