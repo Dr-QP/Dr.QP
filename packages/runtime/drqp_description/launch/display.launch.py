30ecbfd509d58bc95f15185c60a0d86b8bdee25c
Anton Matosov <anton.matosov@gmail.com>
2025-03-09 15:54:36 -0700

    reorg launch file for readability

--- packages/runtime/drqp_description/launch/display.launch.py
+++ packages/runtime/drqp_description/launch/display.launch.py
@@ -32,39 +32,20 @@ def generate_launch_description():
     drqp_description_path = get_package_share_path('drqp_description')
     default_rviz_config_path = drqp_description_path / 'rviz/drqp_description.rviz'
 
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
-                                    description='Flag to enable joint_state_publisher_gui')
-    external_urdf_loc_arg = DeclareLaunchArgument(name='external_urdf_loc', default_value='',
-                                                  description='Absolute path additional urdf \
-                                                    file to include in the model')
-
+    gui = LaunchConfiguration('gui')
     use_sim_time = LaunchConfiguration('use_sim_time')
-    use_sim_time_arg = DeclareLaunchArgument(
-            name='use_sim_time',
-            default_value='true',
-            description='Use sim time if true')
-    rviz_frame_arg = DeclareLaunchArgument(name='rviz_frame',
-                                           default_value='dr_qp/base_link',
-                                           description='Base model frame in rviz')
-    rviz_arg = DeclareLaunchArgument(name='rviz',
-                                     default_value='true',
-                                     choices=['true', 'false'],
-                                     description='Flag to enable rviz')
-    rvizconfig_arg = DeclareLaunchArgument(name='rvizconfig',
-                                           default_value=str(default_rviz_config_path),
-                                           description='Absolute path to rviz config file')
 
     # Depending on gui parameter, either launch joint_state_publisher or joint_state_publisher_gui
     joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        condition=UnlessCondition(LaunchConfiguration('gui'))
+        condition=UnlessCondition(gui)
     )
 
     joint_state_publisher_gui_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
-        condition=IfCondition(LaunchConfiguration('gui'))
+        condition=IfCondition(gui)
     )
 
     rviz_node = Node(
@@ -73,18 +54,36 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=[
-            '-d', LaunchConfiguration('rvizconfig'), '-f', LaunchConfiguration('rviz_frame')],
+            '-d', LaunchConfiguration('rviz_config'),
+            '-f', LaunchConfiguration('rviz_frame')],
         parameters=[{'use_sim_time': use_sim_time}],
         condition=IfCondition(LaunchConfiguration('rviz'))
     )
-
+ 
     return LaunchDescription([
-        gui_arg,
-        rviz_arg,
-        rvizconfig_arg,
-        rviz_frame_arg,
-        external_urdf_loc_arg,
-        use_sim_time_arg,
+        DeclareLaunchArgument(name='gui',
+                              default_value='true',
+                              choices=['true', 'false'],
+                              description='Flag to enable joint_state_publisher_gui'),
+
+        DeclareLaunchArgument(name='external_urdf_loc',
+                              default_value='',
+                              description='Absolute path additional urdf file to include in the model'),
+
+        DeclareLaunchArgument(
+            name='use_sim_time',
+            default_value='true',
+            description='Use sim time if true'),
+        DeclareLaunchArgument(name='rviz',
+                              default_value='true',
+                              choices=['true', 'false'],
+                              description='Flag to enable rviz'),
+        DeclareLaunchArgument(name='rviz_frame',
+                              default_value='dr_qp/base_link',
+                              description='Base model frame in rviz'),
+        DeclareLaunchArgument(name='rviz_config',
+                              default_value=str(default_rviz_config_path),
+                              description='Absolute path to rviz config file'),
         joint_state_publisher_node,
         joint_state_publisher_gui_node,
         rviz_node
