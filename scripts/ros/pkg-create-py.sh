67649bcfc5ff1cb5b59164a55ab5a4dde7f9ca8f
Anton Matosov <anton.matosov@gmail.com>
2025-03-10 10:44:11 -0700

    add script to gen python package

--- scripts/ros/pkg-create-py.sh
+++ scripts/ros/pkg-create-py.sh
@@ -5,8 +5,8 @@ source "$script_dir/__utils.sh"
 
 ros2 pkg create \
   --license MIT \
-  --build-type ament_cmake \
+  --build-type ament_python \
   --maintainer-email anton.matosov@gmail.com \
   --maintainer-name "Anton Matosov" \
-  --destination-directory "$sources_dir" \
+  --destination-directory "$sources_dir/runtime" \
   "$@"
