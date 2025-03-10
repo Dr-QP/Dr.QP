670a9f592dbaf45395222f6aa292ac4e9cc695ba
Anton Matosov <anton.matosov@gmail.com>
2025-03-10 10:44:50 -0700

    add drqp_brain package

---
+++ packages/runtime/drqp_brain/drqp_brain/one_leg_ik.py
@@ -0,0 +1,69 @@
+# Copyright (c) 2017-2025 Anton Matosov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Standard Python imports
+import argparse
+import math
+import sys
+import xml.dom.minidom
+
+# Third-party imports
+import packaging.version
+
+# ROS 2 imports
+from rcl_interfaces.msg import ParameterDescriptor, ParameterType
+import rclpy
+import rclpy.node
+import sensor_msgs.msg
+import std_msgs.msg
+
+
+class RobotIK(rclpy.node.Node):
+
+    def get_param(self, name):
+        return self.get_parameter(name).value
+
+
+
+def main():
+    # Initialize rclpy with the command-line arguments
+    rclpy.init()
+
+    # Strip off the ROS 2-specific command-line arguments
+    stripped_args = rclpy.utilities.remove_ros_args(args=sys.argv)
+    parser = argparse.ArgumentParser()
+
+    # Parse the remaining arguments, noting that the passed-in args must *not*
+    # contain the name of the program.
+    parsed_args = parser.parse_args(args=stripped_args[1:])
+
+    node = RobotIK()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+
+    node.destroy_node()
+    rclpy.try_shutdown()
+
+
+if __name__ == '__main__':
+    main()
