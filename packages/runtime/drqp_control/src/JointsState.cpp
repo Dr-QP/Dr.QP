15ffe0d9a6ce56dd950ba25e4a1c4c0e09ee02c9
Anton Matosov <anton.matosov@gmail.com>
2025-03-09 21:25:53 -0700

    Convert pose_async as well

--- packages/runtime/drqp_control/src/JointsState.cpp
+++ packages/runtime/drqp_control/src/JointsState.cpp
@@ -30,8 +30,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 
-#include <drqp_interfaces/msg/sync_position_command.hpp>
 #include <drqp_interfaces/msg/multi_sync_position_command.hpp>
+#include <drqp_interfaces/msg/multi_async_position_command.hpp>
 
 struct Params
 {
@@ -39,11 +39,11 @@ struct Params
   double ratio = 1.;
 };
 
-static const auto kServoIdToJoint = [](){
+static const auto kServoIdToJoint = []() {
   std::unordered_map<uint8_t, Params> result;
 
   const std::string kRight = "right";
-  const std::array<const char*, kServosPerLeg> kJointNames = { "-coxa", "-femur", "-tibia" };
+  const std::array<const char*, kServosPerLeg> kJointNames = {"-coxa", "-femur", "-tibia"};
   for (const auto& leg : kAllLegServoIds) {
     int jointNameIndex = 0;
     for (const int servoId : leg) {
@@ -51,11 +51,10 @@ static const auto kServoIdToJoint = [](){
       std::string jointName = "dr_qp/" + legName + kJointNames[jointNameIndex];
       std::replace(jointName.begin(), jointName.end(), '-', '_');
 
-      const bool isRight = std::find_end(legName.begin(), legName.end(), kRight.begin(), kRight.end()) != legName.end();
-      result[servoId] = {
-        jointName,
-        (isRight ? -1. : 1.) / 1023.
-      };
+      const bool isRight =
+        std::find_end(legName.begin(), legName.end(), kRight.begin(), kRight.end()) !=
+        legName.end();
+      result[servoId] = {jointName, (isRight ? -1. : 1.) / 1023.};
       ++jointNameIndex;
     }
   }
@@ -72,48 +71,75 @@ public:
     joint_states_publisher_ =
       this->create_publisher<sensor_msgs::msg::JointState>("joint_states", 10);
 
-
     multiSyncPoseSubscription_ =
       this->create_subscription<drqp_interfaces::msg::MultiSyncPositionCommand>(
         "pose", 10, [this](const drqp_interfaces::msg::MultiSyncPositionCommand& msg) {
           try {
-            std::vector<std::string> names;
-            std::vector<double> positions;
+            sensor_msgs::msg::JointState jointState;
+            jointState.header.stamp = this->get_clock()->now();
 
             for (size_t index = 0; index < msg.positions.size(); ++index) {
               auto pos = msg.positions.at(index);
-              if (kServoIdToJoint.count(pos.id) == 0)
-              {
-                continue;
-              }
 
-              const Params params = kServoIdToJoint.at(pos.id);
-              const double positionAsRatio = pos.position * params.ratio;
-              // Position => Radians
-              // 0 => -Pi
-              // 512 => 0
-              // 1023 => Pi
-              const double positionInRadians = positionAsRatio * (2 * 3.14) - 3.14;
-
-              names.push_back(params.name);
-              positions.push_back(positionInRadians);
+              addJointServo(jointState, pos.id, pos.position);
             }
 
-            sensor_msgs::msg::JointState msg;
-            msg.header.stamp = this->get_clock()->now();
-            msg.name = names;
-            msg.position = positions;
-            joint_states_publisher_->publish(msg);
+            joint_states_publisher_->publish(jointState);
           } catch (std::exception& e) {
             RCLCPP_ERROR(get_logger(), "Exception occurred in pose handler %s", e.what());
           } catch (...) {
             RCLCPP_ERROR(get_logger(), "Unknown exception occurred in pose handler.");
           }
         });
+
+    multiAsyncPoseSubscription_ =
+      create_subscription<drqp_interfaces::msg::MultiAsyncPositionCommand>(
+        "pose_async", 10, [this](const drqp_interfaces::msg::MultiAsyncPositionCommand& msg) {
+          try {
+            sensor_msgs::msg::JointState jointState;
+            jointState.header.stamp = this->get_clock()->now();
+
+            for (size_t index = 0; index < msg.positions.size(); ++index) {
+              auto pos = msg.positions.at(index);
+
+              addJointServo(jointState, pos.id, pos.position);
+            }
+
+            joint_states_publisher_->publish(jointState);
+          } catch (std::exception& e) {
+            RCLCPP_ERROR(get_logger(), "Exception occurred in pose_async handler %s", e.what());
+          } catch (...) {
+            RCLCPP_ERROR(get_logger(), "Unknown exception occurred in pose_async handler.");
+          }
+        });
+  }
+
+private:
+  void addJointServo(sensor_msgs::msg::JointState& jointState, ServoId servoId, uint16_t position)
+  {
+    if (kServoIdToJoint.count(servoId) == 0) {
+      RCLCPP_ERROR(get_logger(), "Skipping unknown servo id %i", servoId);
+      return;
+    }
+
+    const Params params = kServoIdToJoint.at(servoId);
+    const double positionAsRatio = position * params.ratio;
+    // Position => Radians
+    // 0 => -Pi
+    // 512 => 0
+    // 1023 => Pi
+    const double positionInRadians = positionAsRatio * (2 * 3.14) - 3.14;
+
+    jointState.name.push_back(params.name);
+    jointState.position.push_back(positionInRadians);
   }
 
   rclcpp::Subscription<drqp_interfaces::msg::MultiSyncPositionCommand>::SharedPtr
     multiSyncPoseSubscription_;
+
+  rclcpp::Subscription<drqp_interfaces::msg::MultiAsyncPositionCommand>::SharedPtr
+    multiAsyncPoseSubscription_;
+
   rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr joint_states_publisher_;
 };
 
