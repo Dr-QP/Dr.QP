419555f75801e0f126cdfc4db11957f19c15716b
Anton Matosov <anton.matosov@gmail.com>
2025-03-12 23:04:24 -0700

    fix linter run

--- scripts/python-reformat.sh
+++ scripts/python-reformat.sh
@@ -4,6 +4,6 @@ script_dir=$(dirname $0)
 source "$script_dir/ros/__utils.sh"
 
 PATH="$PATH:/home/anton/.vscode/extensions/charliermarsh.ruff-2025.14.0-linux-x64/bundled/libs/bin/"
-all_python_files=$(find $sources_dir -name "*.py")
-ruff format $all_python_files
-ruff check --fix $all_python_files
+
+ruff format $sources_dir
+ruff check --fix $sources_dir
