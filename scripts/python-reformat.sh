09de8daaf1c589981240afb5e093829f9bc0c198
Anton Matosov <anton.matosov@gmail.com>
2025-03-13 00:01:31 -0700

    setup venv and use it for ruff

--- scripts/python-reformat.sh
+++ scripts/python-reformat.sh
@@ -3,7 +3,7 @@
 script_dir=$(dirname $0)
 source "$script_dir/ros/__utils.sh"
 
-PATH="$PATH:/home/anton/.vscode/extensions/charliermarsh.ruff-2025.14.0-linux-x64/bundled/libs/bin/"
+source $root_dir/.venv/bin/activate
 
 ruff format $sources_dir
 ruff check --fix $sources_dir
