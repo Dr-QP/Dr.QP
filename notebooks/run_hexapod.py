881b11d0927a53c75fee03cb589d72b202512e20
Anton Matosov <anton.matosov@gmail.com>
2025-04-03 11:40:02 -0700

    add hexapod walking

---
+++ notebooks/run_hexapod.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2017-2025 Anton Matosov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from matplotlib import pyplot as plt
+from models import HexapodModel
+from plotting import plot_hexapod, update_hexapod_plot
+from walk_controller import GaitType, WalkController
+import ps5_controller
+
+drqp_front_offset = 0.116924  # x offset for the front and back legs
+drqp_side_offset = 0.063871  # y offset fo the front and back legs
+drqp_middle_offset = 0.103  # x offset for the middle legs
+
+drqp_coxa = 0.053
+drqp_femur = 0.066225
+drqp_tibia = 0.123
+
+hexapod = HexapodModel(
+    coxa_len=drqp_coxa,
+    femur_len=drqp_femur,
+    tibia_len=drqp_tibia,
+    front_offset=drqp_front_offset,
+    middle_offset=drqp_middle_offset,
+    side_offset=drqp_side_offset,
+    leg_rotation=[0, 0, 45],
+)
+
+hexapod.forward_kinematics(0, -25, 110)
+
+walker = WalkController(
+    hexapod, step_length=120, step_height=60, rotation_speed_degrees=10, gait=GaitType.ripple
+)
+
+
+fig, ax, plot_data = plot_hexapod(hexapod, feet_trails_frames=30)
+
+
+def process_frame(delta_time=0.001):
+    update_hexapod_plot(hexapod, plot_data)
+
+    plt.show(block=False)
+    plt.pause(delta_time)
+
+
+g.process_frame = process_frame
+
+while plt.get_fignums():  # window(s) open
+    inputs = ps5_controller.get_controls()
+    walker.next(
+        stride_direction=inputs.direction,
+        stride_ratio=inputs.walk_speed,
+        rotation_ratio=inputs.rotation_speed,
+    )
+
+    process_frame(0.001)
+
+ps5_controller.close()
