02342f24411b707f44bbf5fdc1a2bafcbc88b3c3
Anton Matosov <anton.matosov@gmail.com>
2025-04-18 20:33:38 -0700

    make pseudo private md files excluded

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,7 +33,7 @@ language = 'en'
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['**/_*.rst', '**/*.ipynb']
+exclude_patterns = ['**/_*.rst', '**/_*.md', '**/*.ipynb']
 
 # -- General configuration
 extensions = [
