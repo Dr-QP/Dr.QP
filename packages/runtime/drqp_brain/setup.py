eeea4be838673c750c13285abd7c3df57bb14fb0
Anton Matosov <anton.matosov@gmail.com>
2025-06-11 17:32:00 +0000

    fix setup.py

--- packages/runtime/drqp_brain/setup.py
+++ packages/runtime/drqp_brain/setup.py
@@ -10,14 +10,12 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    requirements=['numpy', 'scipy'],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='Anton Matosov',
     maintainer_email='anton.matosov@gmail.com',
     description='IK solvers and other high level control algorithms',
     license='MIT',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'run_hexapod = drqp_brain.run_hexapod_ros:main',
