ff82ab89756cbcbc4648a96f87fa59751d4a8e8b
Anton Matosov <anton.matosov@gmail.com>
2025-06-30 16:29:56 -0700

    more corner cases

--- packages/runtime/drqp_control/test/TestRobotConfig.cpp
+++ packages/runtime/drqp_control/test/TestRobotConfig.cpp
@@ -126,6 +126,16 @@ SCENARIO("ROS node")
       {
         CHECK_THROWS(
           robotConfig.loadConfig(TEST_DATA_DIR_IN_SOURCE_TREE "/invalid_robot_config.yml"));
+        CHECK_THROWS(robotConfig.loadConfig(TEST_DATA_DIR_IN_SOURCE_TREE "/non_robot_config.yml"));
+      }
+    }
+
+    WHEN("Config file is not found")
+    {
+      THEN("Loading should throw")
+      {
+        CHECK_THROWS(
+          robotConfig.loadConfig(TEST_DATA_DIR_IN_SOURCE_TREE "/non_existing_config.yml"));
       }
     }
   }
