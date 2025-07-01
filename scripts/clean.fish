ba093afcacf9b676639be26b911518e8278ed3a3
Anton Matosov <anton.matosov@gmail.com>
2025-07-01 16:04:17 -0700

    switch clean to fish

---
+++ scripts/clean.fish
@@ -0,0 +1,10 @@
+#!/usr/bin/env fish
+
+rm -rf **/build/*
+rm -rf **/_build/*
+rm -rf **/install/*
+rm -rf **/log/*
+rm -rf **/lcov/*
+rm -rf **/.cache/*
+rm -rf **/__pycache__/
+rm -rf **/*.egg-info/
