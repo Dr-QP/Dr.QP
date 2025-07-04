3c8d77d362cfcc689c4df2b6865d3d5e2b920449
Anton Matosov <anton.matosov@gmail.com>
2025-07-04 01:04:52 -0700

    implement --update-venv and use it in ci

--- scripts/setup.bash
+++ scripts/setup.bash
@@ -1,6 +1,24 @@
-
-script_dir=$(dirname "${BASH_SOURCE[0]}")
+script_file="${BASH_SOURCE[0]}"
+script_dir=$(dirname "$script_file")
 source "$script_dir/__utils.sh"
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --update-venv)
+      $root_dir/docker/ros/deploy/prod-venv.sh $root_dir/build
+      $root_dir/docker/ros/deploy/prod-venv.sh $root_dir/install
+
+      shift
+      ;;
+    --help)
+      echo "Usage: source $script_file [--update-venv]"
+      echo
+      echo "  --update-venv: Update the virtual environment"
+      return
+      ;;
+  esac
+done
+
 
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 
