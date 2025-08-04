178b8fd14f31f59e4a86406fd598e3fd4e38c145
Anton Matosov <anton.matosov@gmail.com>
2025-08-04 15:23:50 -0700

    add test launch files

---
+++ packages/runtime/drqp_control/launch/test_forward_position_controller.launch.py
@@ -0,0 +1,46 @@
+# Copyright (c) 2017-2025 Anton Matosov
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    position_goals = PathJoinSubstitution(
+        [
+            FindPackageShare('drqp_control'),
+            'config',
+            'drqp_forward_position_publisher.yml',
+        ]
+    )
+
+    return LaunchDescription(
+        [
+            Node(
+                package='ros2_controllers_test_nodes',
+                executable='publisher_forward_position_controller',
+                name='publisher_forward_position_controller',
+                parameters=[position_goals],
+                output='both',
+            )
+        ]
+    )
