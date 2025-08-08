12035fb1fb83c529c5485808c48bfcfe3bbbed8f
Anton Matosov <anton.matosov@gmail.com>
2025-08-08 14:53:47 -0700

    switch back to forward_position_controller

--- packages/runtime/drqp_brain/launch/bringup.launch.py
+++ packages/runtime/drqp_brain/launch/bringup.launch.py
@@ -61,14 +61,14 @@ def generate_launch_description():
     robot_controller_spawner = Node(
         package='controller_manager',
         executable='spawner',
-        arguments=['drqp_position_controller', '--param-file', robot_controllers],
+        arguments=['forward_position_controller', '--param-file', robot_controllers],
     )
 
-    robot_controller_spawner = Node(
-        package='controller_manager',
-        executable='spawner',
-        arguments=['joint_trajectory_position_controller', '--param-file', robot_controllers],
-    )
+    # robot_controller_spawner = Node(
+    #     package='controller_manager',
+    #     executable='spawner',
+    #     arguments=['joint_trajectory_position_controller', '--param-file', robot_controllers],
+    # )
     ############################################################################
 
     return LaunchDescription(
