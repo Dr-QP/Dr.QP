a30125cd79907975fc0bc5e0244899e427f973de
Anton Matosov <anton.matosov@gmail.com>
2025-10-29 22:13:09 -0700

    format

--- packages/runtime/drqp_control/src/RobotConfig.cpp
+++ packages/runtime/drqp_control/src/RobotConfig.cpp
@@ -211,7 +211,8 @@ std::optional<RobotConfig::ServoLimitValues> RobotConfig::getServoLimits(uint8_t
   const JointParams jointParams = servoIdToJoint_.at(servoId);
   const uint16_t minPosition = radiansToPosition(jointParams.min_angle_rads);
   const uint16_t maxPosition = radiansToPosition(jointParams.max_angle_rads);
-  const uint16_t maxPWM = static_cast<uint16_t>(std::round(mapToRange(jointParams.max_torque, 0.0, 1.0, 0, 1023)));
+  const uint16_t maxPWM =
+    static_cast<uint16_t>(std::round(mapToRange(jointParams.max_torque, 0.0, 1.0, 0, 1023)));
 
   return ServoLimitValues{
     .max_pwm = maxPWM,
