4aa5efe4920a2980cfc4c279b1d1497bf04275dd
Anton Matosov <anton.matosov@gmail.com>
2026-02-09 05:41:22 +0000

    fix import of test utils
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- packages/simulation/drqp_gazebo/test/test_robot_control_gazebo.py
+++ packages/simulation/drqp_gazebo/test/test_robot_control_gazebo.py
@@ -19,6 +19,8 @@
 # THE SOFTWARE.
 
 import math
+import os
+import sys
 import time
 import unittest
 
@@ -40,6 +42,8 @@ import rclpy
 from rclpy.qos import QoSDurabilityPolicy, QoSProfile
 from rosgraph_msgs.msg import Clock
 import std_msgs.msg
+
+sys.path.append(os.path.dirname(__file__))
 from test_utils import ensure_gz_sim_not_running
 
 
@@ -422,9 +426,7 @@ class TestGazeboRobotControl(unittest.TestCase):
         # Log movement and verify that some motion occurred along Y
         if self.robot_odometry is not None:
             end_y = self.robot_odometry.pose.pose.position.y
-            self.node.get_logger().info(
-                f'Right movement: start_y={start_y:.3f}, end_y={end_y:.3f}'
-            )
+            self.node.get_logger().info(f'Right movement: start_y={start_y:.3f}, end_y={end_y:.3f}')
             # Require a minimal change in Y to consider the movement successful
             self.assertGreater(
                 abs(end_y - start_y),
