69aaf764ec69c299ece9bd99c20bb632ed40eec0
Anton Matosov <anton.matosov@gmail.com>
2026-02-10 21:36:55 +0000

    move validate_skills
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- packages/development/drqp_validate_agents/setup.py
+++ packages/development/drqp_validate_agents/setup.py
@@ -7,8 +7,7 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -24,6 +23,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'validate_skills = drqp_validate_agents.validate_skills.__main__:main',
         ],
     },
 )
