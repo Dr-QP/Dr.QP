c12d14a350ee8ee953bb26c8fdae63c062a40cc9
Anton Matosov <anton.matosov@gmail.com>
2026-03-02 20:52:06 -0800

    bash superlint warnings
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- scripts/python-reformat.sh
+++ scripts/python-reformat.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-script_dir=$(dirname $0)
+script_dir=$(dirname "$0")
 source "$script_dir/__utils.sh"
 
 notebooks_dir="$root_dir/docs/source/notebooks"
 tool_scripts_dir="$root_dir/scripts"
 
-source $root_dir/.venv/bin/activate
+source "$root_dir"/.venv/bin/activate
 
 ruff_format='ruff format --quiet'
 ruff_lint_fix='ruff check --quiet --fix'
@@ -17,7 +17,7 @@ $ruff_format "${all_sources[@]}"
 $ruff_lint_fix "${all_sources[@]}"
 $ruff_isort "${all_sources[@]}"
 
-script_dir=$(dirname $0)
-$script_dir/sync-notebooks.sh --pipe "$ruff_format {}" --pipe "$ruff_lint_fix --ignore E402,F811 {}" --pipe "$ruff_isort {}" --pipe-fmt "py:percent"
+script_dir=$(dirname "$0")
+"$script_dir"/sync-notebooks.sh --pipe "$ruff_format {}" --pipe "$ruff_lint_fix --ignore E402,F811 {}" --pipe "$ruff_isort {}" --pipe-fmt "py:percent"
 
-ansible-lint --fix $root_dir/docker/ros/ansible
+ansible-lint --fix "$root_dir"/docker/ros/ansible
