8cdaddf70cdff3299aaf7dc428052fcff204a92c
Anton Matosov <anton.matosov@gmail.com>
2026-03-04 05:38:24 +0000

    one step closer to working git
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- .devcontainer/devcontainer-init.sh
+++ .devcontainer/devcontainer-init.sh
@@ -8,3 +8,6 @@ esac
 
 script_dir=$(dirname "${BASH_SOURCE[0]}")
 echo "GIT_REPO=$(realpath "$gitdir")" > "$script_dir/.env"
+
+LOCAL_WORKSPACE_FOLDER=$(realpath "$script_dir/..")
+echo "LOCAL_WORKSPACE_FOLDER=$LOCAL_WORKSPACE_FOLDER" >> "$script_dir/.env"
