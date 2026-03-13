fd2a3aeee7ac990b6bdc9595f6cab1f76a798a56
Anton Matosov <anton.matosov@gmail.com>
2026-03-13 06:58:24 +0000

    tests_require=['pytest'] is still a must have
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- packages/runtime/drqp_brain/setup.py
+++ packages/runtime/drqp_brain/setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(
+    tests_require=['pytest'], # Ensure pytest is declared so colcon does not skip the test/ suite.
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/drqp_brain']),
         ('share/drqp_brain', ['package.xml']),
