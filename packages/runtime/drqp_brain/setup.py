8fd33edd5cc674db4fefe9ec7570fcd3920f0da6
Anton Matosov <anton.matosov@gmail.com>
2026-03-13 06:54:06 +0000

    fix build. total jank...
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- packages/runtime/drqp_brain/setup.py
+++ packages/runtime/drqp_brain/setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup
 
-setup()
+setup(
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/drqp_brain']),
+        ('share/drqp_brain', ['package.xml']),
+        ('share/drqp_brain/launch', ['launch/bringup.launch.py']),
+    ],
+)
--- packages/simulation/drqp_gazebo/setup.py
+++ packages/simulation/drqp_gazebo/setup.py
@@ -1,3 +1,10 @@
 from setuptools import setup
 
-setup()
+setup(
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/drqp_gazebo']),
+        ('share/drqp_gazebo', ['package.xml']),
+        ('share/drqp_gazebo/launch', ['launch/sim.launch.py']),
+        ('share/drqp_gazebo/config', ['config/drqp_gazebo_bridge.yml']),
+    ],
+)
