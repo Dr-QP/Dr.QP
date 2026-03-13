31a71d8e82e56f3d0282de12e25be8cf6d8fde0c
Anton Matosov <anton.matosov@gmail.com>
2026-03-13 23:52:15 +0000

    simplify
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- .devcontainer/devcontainer-init.sh
+++ .devcontainer/devcontainer-init.sh
@@ -1,14 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-gitdir="$(git rev-parse --git-common-dir)"
-case $gitdir in
-    /*) ;;
-    *) gitdir=$PWD/$gitdir
-esac
-
 script_dir=$(dirname "${BASH_SOURCE[0]}")
-echo "GIT_REPO=$(realpath "$gitdir")" > "$script_dir/.env"
+
+gitdir=$(realpath "$(git rev-parse --git-common-dir)")
+echo "GIT_REPO=$gitdir" > "$script_dir/.env"
 
 LOCAL_WORKSPACE_FOLDER=$(realpath "$script_dir/..")
 echo "LOCAL_WORKSPACE_FOLDER=$LOCAL_WORKSPACE_FOLDER" >> "$script_dir/.env"
