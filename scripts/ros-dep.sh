7fd2baee9b18d515b57a4af392b357f0d1222be3
Anton Matosov <anton.matosov@gmail.com>
2026-04-18 23:03:28 +0000

    switch rosdep task to be shell
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- scripts/ros-dep.sh
+++ scripts/ros-dep.sh
@@ -5,5 +5,7 @@ source "$script_dir/__utils.sh"
 
 sudo apt-get update
 
+source "$script_dir/setup.bash"
+
 # -r is needed for arm64 install as it doesn't have gazebo
 rosdep install --from-paths "$sources_dir" --ignore-src -y -r
