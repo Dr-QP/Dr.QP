dc99e6f856391c65b35069dd256e424f3ecfa321
Anton Matosov <anton.matosov@gmail.com>
2026-05-05 04:10:13 +0000

    fix quotes
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- docker/ros/deploy/prod-venv-create.sh
+++ docker/ros/deploy/prod-venv-create.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 script_dir=$(dirname "${BASH_SOURCE[0]}")
-root_dir_default=$(dirname $(dirname $(dirname "$script_dir")))
+root_dir_default="$(dirname "$(dirname "$(dirname "$script_dir")")")"
 root_dir=${1:-$root_dir_default}
 
 if [[ -f "$root_dir/.venv-prod/bin/activate" ]]; then
