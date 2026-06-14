adce23a117f9872be6d038b7f049909b6c502a06
Anton Matosov <anton.matosov@gmail.com>
2026-06-14 18:53:08 +0000

    cleaner clean
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- scripts/clean.fish
+++ scripts/clean.fish
@@ -1,65 +1,21 @@
 #!/usr/bin/env fish
 
 function clean
-  set pattern $argv[1]
-  set files_to_clean (eval "echo $pattern") # This emits the warning. Yet to figure how to do globbing in function
-  if count $files_to_clean >/dev/null
-      echo "Cleaning $pattern"
-      rm -rf $files_to_clean
-  end
+  set files_to_clean $argv[1]
+  echo "Cleaning '$files_to_clean'..."
+  rm -rf "$files_to_clean"
+  echo "Done."
 end
 
-# clean "**/build/*"
-# clean "**/_build/*"
-# clean "**/install/*"
-# clean "**/log/*"
-# clean "**/lcov/*"
-# clean "**/.cache/*"
-# clean "**/__pycache__/"
-# clean "**/*.egg-info/"
+clean '**/build/*'
+clean '**/_build/*'
+clean '**/install/*'
+clean '**/log/*'
+clean '**/lcov/*'
+clean '**/.cache/*'
+clean '**/__pycache__/*'
+clean '**/.pytest_cache/*'
+clean '**/*.egg-info/*'
+clean '**/.tmp'
 
 
-set files_to_clean **/build/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/_build/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/install/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/log/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/lcov/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/.cache/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/__pycache__/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/.pytest_cache/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
-
-set files_to_clean **/*.egg-info/*
-if count $files_to_clean >/dev/null
-    rm -rf $files_to_clean
-end
