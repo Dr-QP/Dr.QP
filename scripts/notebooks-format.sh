ef5df1e5e26a69c2f7a98bb6e8687d5e11acd666
Anton Matosov <anton.matosov@gmail.com>
2026-07-13 00:12:45 +0000

    make notebook reformat accept single files
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- scripts/notebooks-format.sh
+++ scripts/notebooks-format.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-set -euo pipefail
+set -xeuo pipefail
 
 script_dir=$(dirname "$0")
 source "$script_dir/ruff-commands.sh"
 
 script_dir=$(dirname "$0")
-"$script_dir"/notebooks-sync.sh --pipe "$ruff_format {}" --pipe "$ruff_lint_fix --ignore E402,F811 {}" --pipe "$ruff_isort {}" --pipe-fmt "py:percent"
 
+files_to_format="$@"
+if [ -z "$files_to_format" ]; then
+    files_to_format="$(find "$root_dir/docs/source/notebooks" -type f \( -name "*.ipynb" -o -name "*.md" \))"
+fi
+
+jupytext --sync $files_to_format --quiet --pipe "$ruff_format {}" --pipe "$ruff_lint_fix --ignore E402,F811 {}" --pipe "$ruff_isort {}" --pipe-fmt "py:percent"
