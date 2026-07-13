836a60d8f0ce271573a04d708fb0cbf20c5acf0e
Anton Matosov <anton.matosov@gmail.com>
2026-07-13 01:00:09 +0000

    add some troubleshooting
    Signed-off-by: Anton Matosov <anton.matosov@gmail.com>

--- scripts/devcontainer-setup-pre-commit.sh
+++ scripts/devcontainer-setup-pre-commit.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
-set -euo pipefail
+set -xeuo pipefail
 
 script_dir=$(dirname "$0")
 source "$script_dir/__utils.sh"
 
 cd "$root_dir"
+echo "$pwd"
+ls -al ".git"
+ls -al ".git/"
+echo $(command -v git || echo "git not found")
+echo $(command -v pre-commit || echo "pre-commit not found")
 
 # Install the repository's hooks for this checkout. Re-running this command is
 # safe and ensures both staged-file and pre-push checks are available.
